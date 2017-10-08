5bd45fda312ec6d795afcd8a7a707eac1e125c8b
Andrew Basiuk <a.basyuk4779@gmail.com>
2017-10-08 19:10:04 +0300

    new design for work-7

--- work/7/main.js
+++ work/7/main.js
@@ -6,20 +6,20 @@ $(document).ready(function() {
 	function showWeather() {
 		var input = $(".form__input").val(),
 			inputBigLetter = input.charAt(0).toUpperCase() + input.slice(1),
-			weatherFiveDays = "http://api.openweathermap.org/data/2.5/forecast?q=" + inputBigLetter + "&appid=fc3da5f655d9b4c55ce7786120594255&units=metric",
+			weatherFiveDays = 'http://api.openweathermap.org/data/2.5/forecast?q=' + inputBigLetter + '&appid=fc3da5f655d9b4c55ce7786120594255&units=metric',
 			weatherNow = "http://api.openweathermap.org/data/2.5/weather?q=" + inputBigLetter + "&appid=fc3da5f655d9b4c55ce7786120594255&units=metric";
 
-		$('.weather-info__heading').text(inputBigLetter + " weather for 5 days");
+		$('#weather-info__heading').text(inputBigLetter + " weather for 5 days");
 		$('.weather-info__item').remove();
-		$('.weather-info-now__image').remove();
+		$('#weather-info-now__image').remove();
 
 		$.getJSON(weatherNow, function(objektAPI) {
 			var temp = Math.round(objektAPI.main.temp),
 				weatherIcon = objektAPI.weather[0].icon,
 				imgLink = "http://openweathermap.org/img/w/" + weatherIcon + ".png";
 
-			$('.weather-indo-now__text').text("Now in " + inputBigLetter + "  " + temp + "°");
-			$('.weather-indo-now').append("<img src=" + imgLink + " alt='icon' class='weather-info-now__image'>");
+			$('#weather-info-now__text').text("Now in " + inputBigLetter + "  " + temp + "°");
+			$('#weather-info-now').append("<img src=" + imgLink + " alt='icon' id='weather-info-now__image' class='weather-info-now__image'>");
 		});
 
 
@@ -27,31 +27,45 @@ $(document).ready(function() {
 			var dayList = objektAPI.list;
 
 			dayList.forEach( function(item, i) {
-				var temp = Math.round(dayList[i*8+6].main.temp),
-					 date = dayList[i*8+6].dt_txt.slice(0, 10),
-					 weatherIcon = dayList[i*8+6].weather[0].icon,
+				var selection = i*8+6;
+
+				var temp = Math.round(dayList[selection].main.temp),
+					 date = dayList[selection].dt_txt.slice(0, 10),
+					 weatherIcon = dayList[selection].weather[0].icon,
 					 imgLink = "http://openweathermap.org/img/w/" + weatherIcon + ".png",
 					 dateParagraph = "<p class='weather-info__date'>" + date + "</p>",
 					 tempParagraph = "<p class='weather-info__paragraph'>" + temp +  "°" + "</p>",
 					 image = "<img src=" + imgLink + " alt='icon' class='weather-info__image'>",
 					 weatherValue = "<div class='weather-info__value'>" + tempParagraph + image + "</div>",
-					 el = "<li class='weather-info__item'>" + dateParagraph + weatherValue + "</li>";
+					 el = "<li id='weather-info__item' class='weather-info__item'>" + dateParagraph + weatherValue + "</li>";
 
-				return $('.weather-info-list').append(el);
+				return $('#weather-info-list').append(el);
 			});
 		});
 	}
 
-	$(".form__button").click(function() {
+	// $(".form__button").click(function() {
+	// 	showWeather();
+	// });
+
+	// $(".form__input").keypress(function(e){
+	//    if(e.keyCode==13){
+	//      	showWeather();
+	//    }
+	// });
+
+	$("form").submit(function(e) {
 		showWeather();
+		e.preventDefault();
 	});
-
-	$(".form__input").keypress(function(e){
-	   if(e.keyCode==13){
-	     	showWeather();
-	   }
-	});
-
-
 
 });
+
+
+// axios.post(weatherNow)
+//   .then(function (response) {
+//     console.log("response");
+//   })
+//   .catch(function (error) {
+//     console.log("error");
+//   });
